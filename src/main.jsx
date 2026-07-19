fda821e3a1c3376fd28b3f0d82acaacbe06fb0df
lthomaswestcoast <lthomaswestcoast@gmail.com>
2026-07-18 18:08:17 -0700

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import QuoteGenerator from "./QuoteGenerator.jsx";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <QuoteGenerator />
+  </React.StrictMode>
+);
